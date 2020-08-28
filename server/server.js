46e3b139c7fec56c620af1ae55c10f675e5fb28a
raphapc <furacker@gmail.com>
2020-08-28 23:51:05 +0100

    Adding postMessage mutation

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const { GraphQLServer } = require('graphql-yoga');
 
+const messages = [];
+
 const typeDefs = `
   type Message {
     id: ID!
@@ -10,15 +12,30 @@ const typeDefs = `
   type Query {
     messages: [Message!]
   }
+
+  type Mutation {
+    postMessage(user: String!, content: String!): ID!
+  }
 `;
 
 const resolvers = {
   Query: {
     messages: () => messages,
   },
+  Mutation: {
+    postMessage: (parent, { user, content }) => {
+      const id = messages.length;
+      messages.push({
+        id,
+        user,
+        content,
+      });
+      return id;
+    },
+  },
 };
 
 const server = new GraphQLServer({ typeDefs, resolvers });
 server.start(({ port }) => {
-  console.log('Server started on port {$port}');
+  console.log('Server started on http://localhost:' + port);
 });
