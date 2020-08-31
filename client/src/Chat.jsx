899ec91815c77e2adef4909650f017632e14a945
raphapc <furacker@gmail.com>
2020-08-31 23:55:11 +0100

    Add gql query and the base to messages component

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,17 +1,65 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloProvider, InMemoryCache, useQuery, gql } from '@apollo/client';
 import React from 'react';
+import { Container } from 'shards-react';
 
 const client = new ApolloClient({
-  uri: 'https://localhost:4000',
+  uri: 'http://localhost:4000/',
   cache: new InMemoryCache(),
 });
 
-const Chat = () => {
-  return <div>Testing Chat</div>;
+const GET_MESSAGES = gql`
+  query {
+    messages {
+      id
+      content
+      user
+    }
+  }
+`;
+
+const Messages = ({ user }) => {
+  const { data } = useQuery(GET_MESSAGES);
+  if (!data) {
+    return null;
+  }
+
+  return (
+    <>
+      {data.messages.map(({ id, user: messageUser, content }) => (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: user === messageUser ? 'flex-end' : 'flex-start',
+            paddingBottom: '1em',
+          }}
+        >
+          <div
+            style={{
+              background: user === messageUser ? '#58bf56' : '#e5e6ea',
+              color: user === messageUser ? 'white' : 'black',
+              padding: '1em',
+              borderRadius: '1em',
+              maxWidth: '60%',
+            }}
+          >
+            {content}
+          </div>
+        </div>
+      ))}
+    </>
+  );
 };
 
-export default () => {
+const Chat = () => {
+  return (
+    <Container>
+      <Messages user="Rapha"></Messages>
+    </Container>
+  );
+};
+
+export default () => (
   <ApolloProvider client={client}>
     <Chat />
-  </ApolloProvider>;
-};
+  </ApolloProvider>
+);
